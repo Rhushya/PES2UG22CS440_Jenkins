be49ff5275f8cffbb9cf6a2a4b0d8e460592fef3
Rhushya <rhushya2004@gmail.com>
2025-03-13 20:27:39 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << " Hello \n";
+  cout << " Hello this is upadted\n";
   return 0;
 }
